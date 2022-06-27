0239ab3b9a1d9e482257eb70db7f25728a76755a
Idan Lev <idan.93lev@gmail.com>
2022-06-27 19:06:43 +0300

    env ,gitignore, dependencies

---
+++ server/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/routes.js");
+
+const uri = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(userRoutes);
+
+mongoose.connect(uri, () => console.log("connected to mongoDB"));
+
+app.listen(PORT, () => {
+  console.log(`server runs on port ${PORT}`);
+});
